5b09a0b645d3a2931ab64bf2fcff17ef42120fa3
Jonathan Alphonso <zoltan3210@Hotmail.com>
2023-05-29 17:51:05 -0400

    fixed example api

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -14,9 +14,9 @@ export const exampleRouter = createTRPCRouter({
       };
     }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+  // getAll: publicProcedure.query(({ ctx }) => {
+  //   return ctx.prisma.example.findMany();
+  // }),
 
   getSecretMessage: protectedProcedure.query(() => {
     return "you can now see this secret message!";
