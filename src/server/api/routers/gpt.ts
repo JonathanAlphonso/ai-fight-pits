971c217c3bbafccd66e8d37c01c5211bec31ccc2
Jonathan Alphonso <zoltan3210@Hotmail.com>
2023-07-24 20:42:00 -0400

    Better timeout 9.9 sec

--- src/server/api/routers/gpt.ts
+++ src/server/api/routers/gpt.ts
@@ -45,7 +45,7 @@ export const gptRouter = createTRPCRouter({
               max_tokens: 400,
             })
             .then((response) => response.data as CreateChatCompletionResponse),
-          new Promise((_, reject) => setTimeout(reject, 9800, "Timeout Error")),
+          new Promise((_, reject) => setTimeout(reject, 9900, "Timeout Error")),
         ]);
 
         if (res && (res as CreateChatCompletionResponse).choices) {
