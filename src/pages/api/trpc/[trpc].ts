698c72e18fb88d16437898d7ce2123e0eb34188d
Jonathan Alphonso <zoltan3210@Hotmail.com>
2023-08-14 17:44:04 -0400

    should be ok now

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,21 +1,18 @@
-//modified to run on vercel edge
-//https://www.charlesharris.dev/blog/run-trpc-on-the-edge
-
-import { createTRPCContext } from "~/server/api/trpc";
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { type NextRequest } from "next/server";
+import { createTRPCContext } from "~/server/api/trpc";
 
-export const config = {
-  runtime: "edge",
-  region: "cle1",
-};
-
-export default async function handler(req: NextRequest) {
-  return fetchRequestHandler({
-    endpoint: "/api/trpc",
-    router: appRouter,
-    req,
-    createContext: createTRPCContext,
-  });
-}
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createTRPCContext,
+  onError:
+    env.NODE_ENV === "development"
+      ? ({ path, error }) => {
+          console.error(
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+          );
+        }
+      : undefined,
+});
